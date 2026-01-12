585b5efd69ddfec204b6c4208f712f55656d2f8d
SirChickenBro <freddiewashington5555@gmail.com>
2026-01-11 21:11:06 -0700

    committing time I suppose

---
+++ something_I_suppose/Assets/Player.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * 0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * 0.5f;
+        }
+    }
+}
